0864435f6305876bc00c90461274f787ec4a400f
Olivia Marunde <omarunde8029@jh-mba158-10.d214.org>
2017-10-04 08:15:48 -0500

    Initial Commit

---
+++ College Profile Builder (Core Data)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  College Profile Builder (Core Data)
+//
+//  Created by Olivia Marunde on 10/4/17.
+//  Copyright © 2017 Olivia Marunde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
